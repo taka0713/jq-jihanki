8bf82a1100e8e1e6f2460c66ad787d7568a9efb9
taka0713 <tyunisu2018@gmail.com>
2022-06-02 10:45:58 +0900

    20220602

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 $(function(){
+
+    $('#start').click(function(){
+
+        let minutes = Number($('#minutes').val());
+        let seconds = Number($('#seconds').val());
+        timer = setInterval(function(){
+            seconds = seconds - 1;
+            $('#seconds').val(seconds);
+            if(seconds <= 0){
+                $('#seconds').val(0);
+            }
+            if(seconds < 0 && minutes > 0){
+                minutes = minutes - 1;
+                seconds = 59;
+                $('#minutes').val(minutes);
+                $('#seconds').val(59);
+            }
+            
+            if(seconds === 0 && minutes === 0){
+                alert('終了です');
+            }
+        },1000);
+
+    });
+
+    $('#stop').click(function(){
+        clearInterval(timer);
+        
+    });
     
 });
