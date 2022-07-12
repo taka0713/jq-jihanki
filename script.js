b784c7dffa6a291f1f5c563c80944fc5cc81d553
taka0713 <tyunisu2018@gmail.com>
2022-07-13 00:46:52 +0900

    message

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ $(function () {
   $("#button_thousand").on("click", function () {
     if ($("#money_thou").val() > 0) {
       $("#money_thou").val($("#money_thou").val() - 1);
+      $("#money_fifty").text($("#money_fifty").text() - 1000);
+      $("#count").text(Number($("#count").text()) + 1000);
+      console.log(typeof $("#count").text());
     }
 
     console.log($("#money_thou").val() - 1);
