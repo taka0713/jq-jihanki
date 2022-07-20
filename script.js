72de3a07765618a5d7ab9ef48eaae36322399000
taka0713 <tyunisu2018@gmail.com>
2022-07-21 01:30:50 +0900

    message

--- script.js
+++ script.js
@@ -10,22 +10,29 @@ $(function () {
   });
 
   $(".juice_button").on("click", function () {
-    twelve_remain = Number($("#twelve_remain").val());
-    buy_count = Number($("#count").text());
-    console.log(buy_count);
-    remain_count = Number($("#remain_count").val());
-    side_mituya = Number($("#side_mituya").text());
+    // twelve_remain = Number($("#twelve_remain").val());
+    // buy_count = Number($("#count").text());
+    // console.log(buy_count);
+    // remain_count = Number($("#remain_count").val());
+    // side_mituya = Number($("#side_mituya").text());
 
-    if ($(this).prev("input").val() > 0 && $("#count").text() >= 120) {
-      twelve_remain = twelve_remain - 1;
-      remain_count = remain_count - 1;
-      side_mituya = side_mituya + 1;
+    const stock = $("#" + $(this).prop("id") + "_remain").val();
 
-      buy_count = buy_count - 120;
-      $("#twelve_remain").val(twelve_remain);
-      $("#remain_count").val(remain_count);
-      $("#side_mituya").text(side_mituya);
-      $("#count").text(buy_count);
-    }
+    stock = stock - 1;
+    Number($("#remain_count").val()) - 1;
+    console.log($(".side_count").prop("id").text() + 1);
+    $(".side_count").prop("id").text() + 1;
+
+    // if ($(this).prev().val() > 0 && $("#count").text() >= 120) {
+    //   twelve_remain = twelve_remain - 1;
+    //   remain_count = remain_count - 1;
+    //   side_mituya = side_mituya + 1;
+
+    //   buy_count = buy_count - 120;
+    //   $("#twelve_remain").val(twelve_remain);
+    //   $("#remain_count").val(remain_count);
+    //   $("#side_mituya").text(side_mituya);
+    //   $("#count").text(buy_count);
+    // }
   });
 });
