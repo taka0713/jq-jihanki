56ebf11252319f0c49d14232188bac88246b07a7
taka0713 <tyunisu2018@gmail.com>
2022-08-28 17:01:35 +0900

    message

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 $(function () {
-  let roulette_flag = 0;
-  let random = 0;
-  $(".juice_button").prop("disabled", true);
-
-  const roulette_hit = [11, 22, 33, 44, 55, 66, 77, 88, 99];
-
   const rensou = {
     twelve: "side_mituya",
     fourteen: "side_pon",
@@ -17,168 +11,102 @@ $(function () {
   };
   $(".money_button").on("click", function () {
     if ($(this).prev("input").val() > 0) {
+      $("#money_fifty").text($("#money_fifty").text() - $(this).data("money"));
+      $("#count").text(Number($("#count").text()) + $(this).data("money"));
       $(this)
         .prev("input")
         .val($(this).prev("input").val() - 1);
-      $("#money_fifty").text($("#money_fifty").text() - $(this).data("money"));
-      $("#count").text(Number($("#count").text()) + $(this).data("money"));
     }
   });
-
   $(".juice_button").on("click", function () {
     if (
-      $("#count").text() - $(this).data("price") >= 0 &&
+      $("#count").text() > 0 &&
       $("#" + $(this).prop("id") + "_remain").val() > 0
     ) {
-      $("#count").text(Number($("#count").text()) - $(this).data("price"));
       let stock = $("#" + $(this).prop("id") + "_remain").val();
       $("#" + $(this).prop("id") + "_remain").val(stock - 1);
+      $("#count").text($("#count").text() - $(this).data("price"));
+      $("#remain_count").val($("#remain_count").val() - 1);
+      console.log($(rensou[$(this).prop("id")]).text());
       $(rensou[$(this).prop("id")]).text(
         Number($(rensou[$(this).prop("id")]).text()) + 1
       );
     }
-    if (Number($("#" + $(this).prop("id") + "_remain").val()) === 0) {
-      $(this).text("売り切れ");
-      $(this).prop("disabled", true);
-    }
-    if (roulette_flag === 0) {
-      roulette_number = Math.floor(Math.random() * 99) + 1;
-      $("#roulette").text(roulette_number);
-      if (roulette_hit.indexOf(roulette_number) !== -1) {
-        roulette_flag = 1;
-        if ($("#" + $(this).prop("id") + "_remain").val() > 0) {
-          $(".juice_button").map(function () {
-            $(this).prop("disabled", false);
-          });
-          if ($("#" + $(this).prop("id") + "_remain").val() === 0) {
-            $(".juice_button").map(function () {
-              $(this).prop("disabled", true);
-            });
-          }
-        }
-      } else {
-        roulette_flag = 0;
-        $("#roulette").text(0);
-      }
-    }
-  });
-
-  $("#reset").on("click", function () {
-    let total_money = $("#money_fifty").text() + $("#count").text();
-    $("#money_thou").val(Math.floor(total_money / 1000));
-    $("#money_ten").val(Math.floor(total_money % 1000) / 100);
-    $("#money_one").val(Math.floor(total_money % 1000) % 100) / 10;
-    $("#money_fifty").text(total_money);
-    $("#roulette").text(0);
-    $("#count").text(0);
   });
 });
+//   let roulette_flag = 0;
+//   let random = 0;
+//   $(".juice_button").prop("disabled", true);
 
-// let roulette_flag = 0;
-// let roulette_number = 0;
+//   const roulette_hit = [11, 22, 33, 44, 55, 66, 77, 88, 99];
 
-// const roulette_hit = [11, 22, 33, 44, 55, 66, 77, 88, 99];
-
-// const rensou = {
-//   twelve: "#side_mituya",
-//   fourteen: "#side_pon",
-//   fifteen: "#side_toropi",
-//   eighteen: "#side_tea",
-//   ten: "#side_lemon",
-//   twenty: "#side_nekuta",
-//   thirty: "#side_cola",
-//   fifty: "#side_pain",
-// };
-
-// $("#roulette").text(0);
-
-// $(".juice_button").prop("disabled", true);
-
-// let juice_price = () => {
-//   $(".juice_button").map(function () {
-//     $(this).prop("disabled", $("#count").text() < $(this).data("price"));
-
-//     if (Number($("#" + $(this).data("id") + "_remain").val()) === 0) {
-//       $(this).prop("disabled", true);
+//   const rensou = {
+//     twelve: "side_mituya",
+//     fourteen: "side_pon",
+//     fifteen: "side_toropi",
+//     eighteen: "side_tea",
+//     ten: "side_lemon",
+//     twenty: "side_nekuta",
+//     thirty: "side_cola",
+//     fifty: "side_pain",
+//   };
+//   $(".money_button").on("click", function () {
+//     if ($(this).prev("input").val() > 0) {
+//       $(this)
+//         .prev("input")
+//         .val($(this).prev("input").val() - 1);
+//       $("#money_fifty").text($("#money_fifty").text() - $(this).data("money"));
+//       $("#count").text(Number($("#count").text()) + $(this).data("money"));
 //     }
 //   });
-// };
-// let juice_reset = () => {
-//   $(".juice_button").map(function () {
-//     $(this).prop("disabled", true);
-//   });
-//   roulette_flag = 0;
-// };
-// $(".money_button").on("click", function () {
-//   if ($(this).prev("input").val() > 0) {
-//     $(this)
-//       .prev("input")
-//       .val($(this).prev("input").val() - 1);
-//     $("#money_fifty").text($("#money_fifty").text() - $(this).data("money"));
-//     $("#count").text(
-//       Number($("#count").text()) + Number($(this).data("money"))
-//     );
-//   }
 
-//   juice_price();
-// });
-
-// $(".juice_button").on("click", function () {
-//   if (
-//     $("#" + $(this).prop("id") + "_remain").val() > 0 &&
-//     $("#count").text() - $(this).data("price") >= 0
-//   ) {
-//     let stock = $("#" + $(this).prop("id") + "_remain").val();
-//     $("#" + $(this).prop("id") + "_remain").val(stock - 1);
-//     $("#remain_count").val($("#remain_count").val() - 1);
-//     if (roulette_flag === 0) {
-//       $("#count").text($("#count").text() - $(this).data("price"));
+//   $(".juice_button").on("click", function () {
+//     if (
+//       $("#count").text() - $(this).data("price") >= 0 &&
+//       $("#" + $(this).prop("id") + "_remain").val() > 0
+//     ) {
+//       $("#count").text(Number($("#count").text()) - $(this).data("price"));
+//       let stock = $("#" + $(this).prop("id") + "_remain").val();
+//       $("#" + $(this).prop("id") + "_remain").val(stock - 1);
+//       $(rensou[$(this).prop("id")]).text(
+//         Number($(rensou[$(this).prop("id")]).text()) + 1
+//       );
 //     }
-//     $(rensou[$(this).prop("id")]).text(
-//       Number($(rensou[$(this).prop("id")]).text()) + 1
-//     );
-
 //     if (Number($("#" + $(this).prop("id") + "_remain").val()) === 0) {
 //       $(this).text("売り切れ");
 //       $(this).prop("disabled", true);
 //     }
-//     juice_price();
-//   }
-//   if (roulette_flag === 0) {
-//     roulette_number = Math.floor(Math.random() * 99) + 1;
-//     $("#roulette").text(roulette_number);
-//     if (roulette_hit.indexOf(roulette_number) !== -1) {
-//       roulette_flag = 1;
-//       if ($("#" + $(this).prop("id") + "_remain").val() > 0) {
-//         $(".juice_button").map(function () {
-//           $(this).prop("disabled", false);
-//           if (Number($("#" + $(this).prop("id") + "_remain").val()) === 0) {
-//             $(this).prop("disabled", true);
+//     if (roulette_flag === 0) {
+//       roulette_number = Math.floor(Math.random() * 99) + 1;
+//       $("#roulette").text(roulette_number);
+//       if (roulette_hit.indexOf(roulette_number) !== -1) {
+//         roulette_flag = 1;
+//         if ($("#" + $(this).prop("id") + "_remain").val() > 0) {
+//           $(".juice_button").map(function () {
+//             $(this).prop("disabled", false);
+//           });
+//           if ($("#" + $(this).prop("id") + "_remain").val() === 0) {
+//             $(".juice_button").map(function () {
+//               $(this).prop("disabled", true);
+//             });
 //           }
-//         });
+//         }
+//       } else {
+//         roulette_flag = 0;
+//         $("#roulette").text(0);
 //       }
 //     }
-//   } else {
-//     $("#roulette").text(0);
-//     roulette_flag = 0;
-//   }
-// });
-// $("#button_first").on("click", function () {
-//   $(".all_reset").map(function () {
-//     $(this).val($(this).data("default"));
-//     $(this).text($(this).data("default"));
 //   });
-//   juice_reset();
-// });
-// $("#reset").on("click", function () {
-//   let total_money =
-//     Number($("#money_fifty").text()) + Number($("#count").text());
-//   $("#money_thou").val(Math.floor(total_money / 1000));
-//   $("#money_ten").val(Math.floor((total_money % 1000) / 100));
-//   $("#money_one").val(Math.floor(((total_money % 1000) % 100) / 10));
-//   $("#money_fifty").text(total_money);
-//   juice_reset();
-//   $("#count").text(0);
+
+//   $("#reset").on("click", function () {
+//     let total_money = $("#money_fifty").text() + $("#count").text();
+//     $("#money_thou").val(Math.floor(total_money / 1000));
+//     $("#money_ten").val(Math.floor(total_money % 1000) / 100);
+//     $("#money_one").val(Math.floor(total_money % 1000) % 100) / 10;
+//     $("#money_fifty").text(total_money);
+//     $("#roulette").text(0);
+//     $("#count").text(0);
+//   });
 
 //   $(".money_thou").on("click", function () {
 //     if ($("#money_fifty").text() > 0 && $("#money_thou").val() > 0) {
